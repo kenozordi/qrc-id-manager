b497921ba41196c3fc263a35ba02d2b7a2e6b697
kenozordi <kennyozordi@gmail.com>
2022-01-12 11:58:32 +0100

    redirect homepage to admin login

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.layout.default');
+    return redirect(route('admin.login'));
 });
 
 // Admin Routes
